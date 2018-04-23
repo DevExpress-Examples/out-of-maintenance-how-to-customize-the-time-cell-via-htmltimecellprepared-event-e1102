c837ec39d12b52ffab33e6dff29cf02a493a377b
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ VB/WebSite/Default.aspx
@@ -0,0 +1,35 @@
+﻿<%@ Page Language="vb" AutoEventWireup="true" CodeFile="Default.aspx.vb" Inherits="_Default" %>
+<%@ Register Assembly="DevExpress.Web.v15.2" Namespace="DevExpress.Web" TagPrefix="dxm" %>
+
+<%@ Register Assembly="DevExpress.XtraScheduler.v15.2.Core, Version=15.2.0.0, Culture=neutral, PublicKeyToken=79868b8147b5eae4"
+    Namespace="DevExpress.XtraScheduler" TagPrefix="cc1" %>
+
+<%@ Register Assembly="DevExpress.Web.ASPxScheduler.v15.2" Namespace="DevExpress.Web.ASPxScheduler"
+    TagPrefix="dxwschs" %>
+
+<%@ Register Src="~/DefaultObjectDataSources.ascx" TagName="DefaultObjectDataSource" TagPrefix="dds" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" >
+<head id="Head1" runat="server">
+    <title>Untitled Page</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <dds:DefaultObjectDataSource runat="server" ID="DataSource1" />
+        <dxwschs:ASPxScheduler ID="ASPxScheduler1" runat="server" GroupType="Resource" Start="2008-07-24" OnHtmlTimeCellPrepared="OnHtmlTimeCellPrepared" >
+            <Views>
+                <DayView ShowWorkTimeOnly="True"><TimeRulers>
+                            <cc1:TimeRuler></cc1:TimeRuler>
+                       </TimeRulers>
+                </DayView>
+                <WorkWeekView Enabled="false"></WorkWeekView>
+                <MonthView Enabled="false"></MonthView>
+                <TimelineView Enabled="false"></TimelineView>
+            </Views>     
+        </dxwschs:ASPxScheduler>
+    </div>
+    </form>
+</body>
+</html>
---
+++ VB/WebSite/Default.aspx.vb
@@ -0,0 +1,51 @@
+﻿Imports System
+Imports System.Data
+Imports System.Configuration
+Imports System.Collections
+Imports System.Web
+Imports System.Web.Security
+Imports System.Web.UI
+Imports System.Web.UI.WebControls
+Imports System.Web.UI.WebControls.WebParts
+Imports System.Web.UI.HtmlControls
+Imports System.Drawing
+#Region "#1"
+Imports DevExpress.Web.ASPxScheduler
+Imports DevExpress.Web.Internal
+Imports DevExpress.XtraScheduler
+#End Region ' #1
+
+Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As EventArgs)
+        DataHelper.SetupMappings(ASPxScheduler1)
+        DataHelper.ProvideRowInsertion(ASPxScheduler1, DataSource1.AppointmentDataSource)
+        DataSource1.AttachTo(ASPxScheduler1)
+    End Sub
+
+#Region "#2"
+    Protected Sub OnHtmlTimeCellPrepared(ByVal sender As Object, ByVal e As ASPxSchedulerTimeCellPreparedEventArgs)
+        Dim cellInterval As TimeInterval = e.Interval
+        If e.View.Type = DevExpress.XtraScheduler.SchedulerViewType.Day Then
+            Dim dinnerStart As New TimeSpan(15, 0, 0)
+            Dim dinnerEnd As New TimeSpan(17, 0, 0)
+            Dim cellStart As TimeSpan = cellInterval.Start.TimeOfDay
+            Dim cellEnd As TimeSpan = cellInterval.End.TimeOfDay
+            If cellStart >= dinnerStart AndAlso cellStart <= dinnerEnd AndAlso cellEnd <= dinnerEnd Then
+                e.Cell.BackColor = Color.Red
+                Dim valueString As String = HtmlConvertor.ToHtml(Color.Yellow)
+                e.Cell.Style.Add("border-bottom-color", valueString)
+                e.Cell.Style.Add("text-align", "center")
+                e.Cell.Controls.Add(New LiteralControl("dinner"))
+            End If
+        End If
+        If e.View.Type = SchedulerViewType.Week Then
+            Dim specialDate As New Date(2008, 7, 24)
+            If cellInterval.Start = specialDate Then
+                e.Cell.BackColor = Color.Green
+            End If
+        End If
+    End Sub
+#End Region ' #2
+End Class
